d391730562fe214ac333f00777bddb021fb1f6ee
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-06 20:21:28 +0900

    fixed same hole issue

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-  const holes = document.querySelectorAll('.hole');
-  const scoreBoard = document.querySelector('.score');
-  const moles = document.querySelectorAll('.mole');
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const index = Math.floor(Math.random() * holes.length);
+  const hole = holes[index];
+  if (hole === lastHole) {
+    console.log('same hole');
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  console.log(time);
+  console.log(hole);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+  }, time);
+}

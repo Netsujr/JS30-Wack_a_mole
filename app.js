32284e53dcc9fe4c3677ea3857d1a4b19ed53ee3
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-06 20:34:24 +0900

    challenge finished
    need to ad score leaderboard

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+let timeUp = false;
+let score = 0;
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -11,7 +13,7 @@ function randomHole(holes) {
   const index = Math.floor(Math.random() * holes.length);
   const hole = holes[index];
   if (hole === lastHole) {
-    console.log('same hole');
+    // console.log('same hole');
     return randomHole(holes);
   }
   lastHole = hole;
@@ -21,10 +23,28 @@ function randomHole(holes) {
 function peep() {
   const time = randomTime(200, 1000);
   const hole = randomHole(holes);
-  console.log(time);
-  console.log(hole);
+  // console.log(time);
+  // console.log(hole);
   hole.classList.add('up');
   setTimeout(() => {
     hole.classList.remove('up');
+    if (!timeUp) peep();
   }, time);
 }
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => timeUp = true, 10000);
+}
+
+function bonk(event) {
+  if (!event.isTrusted) return 'Cheater!';
+  score++;
+  this.parentNode.classList.remove('up');
+  scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', bonk));
